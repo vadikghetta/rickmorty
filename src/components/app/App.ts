1e8dafe663be033d166d9647f1426b5a9641a4c0
vadikghetta <helloiamvadik@mail.ru>
2023-10-22 21:09:44 +0300

    add animation to headers

--- src/components/app/App.ts
+++ src/components/app/App.ts
@@ -1,11 +1,13 @@
 import "./App.scss"
 import Characters from "../characters"
 import Promo from "../promo"
+import gsap from "gsap"
 
 
 class App {
-    
+    t = gsap.timeline()
    async render() {
+      gsap.from("main", {opacity : 0, duration : 2, ease : "linear"})
       Promo.init()
     await Characters.render()
    } 
--- src/components/promo/index.ts
+++ src/components/promo/index.ts
@@ -9,13 +9,14 @@ interface IPromoTypes {
 class Promo implements IPromoTypes {
     parent: HTMLElement | null; 
     title: HTMLHeadingElement | null;
+    t = gsap.timeline()
     constructor() {
-        this.parent = document.getElementById("promo")
-        this.title = document.querySelector(".title")
+        
     }
 
     init() {
-        gsap.from('.title', {duration : 1, x: -200, opacity : 0})
+        gsap.from('.title', {duration : 1, x: -200, opacity : 0 }),
+        gsap.from('.subtitle', {duration : 1, y: 200, opacity : 0, delay : 1.5 })
     }
 }
 
